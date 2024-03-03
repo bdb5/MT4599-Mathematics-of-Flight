483914f4fb20b105bbc52d9eb7a7b4eef3205897
bdb5 <126100963+bdb5@users.noreply.github.com>
2024-03-03 22:01:26 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,25 +5,15 @@ Created on Sun Mar  3 20:57:51 2024
 
 @author: benbroughton
 """
+
+
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.interpolate import CubicSpline
 
-
-st.write("hello world")
-y_points = [1, 1.5, 0.8, 0.5]
-x_points = [0, 0.33, 0.66, 1]
-
-f = CubicSpline(x_points, y_points, bc_type='natural')
-x_new = np.arange(0, 1.01, 0.01)
-y_new = f(x_new)
-
-fig =  plt.figure()
-plt.plot(x_new, y_new, 'b')
-plt.plot(x_points, y_points, 'ro')
-plt.title('Cubic Spline Test for Pressure Distribution')
-plt.xlabel('x')
-plt.ylabel('y')
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
 
 st.pyplot(fig)
+
