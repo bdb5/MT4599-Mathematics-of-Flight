b0d49da8932de495587f6e54220401b517d7768d
bdb5 <126100963+bdb5@users.noreply.github.com>
2024-03-03 22:56:52 +0000

    Test Again

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,12 @@ Created on Sun Mar  3 20:57:51 2024
 
 import streamlit as st
 import numpy as np
-import matplotlib as plt
+
+from scipy import constants
+
+print(constants.liter)
+
+
+#import matplotlib as plt
 st.write('i hate you', np.pi)
 
