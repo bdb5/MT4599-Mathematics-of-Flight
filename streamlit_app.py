f2d9532ed9d9dd936b41cc3f7229d4a3dc7b1bae
bdb5 <126100963+bdb5@users.noreply.github.com>
2024-03-03 22:53:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,7 @@ Created on Sun Mar  3 20:57:51 2024
 
 
 import streamlit as st
-import matplotlib.pyplot as plt
 import numpy as np
 
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
+st.write('i hate you')
 
