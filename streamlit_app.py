1f15835a7da9e2cfa59bc632a921692af9dc56bc
bdb5 <126100963+bdb5@users.noreply.github.com>
2024-03-03 21:03:35 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,5 @@ Created on Sun Mar  3 20:57:51 2024
 
 @author: benbroughton
 """
-import matplotlib.pyplot as plt
-import numpy as np, numpy
-import scipy as scipy
-import pandas as pd
-import matplotlib.ticker as ticker 
-from matplotlib.ticker import AutoMinorLocator, FixedLocator
-import scipy.integrate as it
 
 print('hello world')
